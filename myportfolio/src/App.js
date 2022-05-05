5a4f8c9cdb2d90484c4cc4d9d0eb5397a555253a
Sanchir <sanchir0607@gmail.com>
2022-05-06 02:25:58 +0800

    secondcommit

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,10 +1,11 @@
-
+import Main from "./components/Main"
 import './App.css';
 
 function App() {
   return (
    <div>
-     <h1>HEllo World</h1>
+     <Main/>
+
    </div>
   );
 }
--- myportfolio/src/components/main.jsx
+++ myportfolio/src/components/main.jsx
@@ -1,3 +1,39 @@
-import React from "react";
+import React from 'react'
 import "../style/style.css"
-import ""
+import {Container, Row, Col} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const main = () => {
+  return (
+    <Container>
+    <Row>
+      <Col>
+      <div className='image'>
+        <h1>IMAGE</h1>
+
+
+        </div></Col>
+      
+    </Row>
+    <Row>
+      <Col><div className='intro'>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae praesentium voluptatibus, minus voluptates nobis iure molestiae.</h1>
+        </div></Col>
+      
+    </Row>
+    <Row>
+      <Col><div className='desc'>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis beatae reiciendis quasi animi nam doloribus corporis voluptatibus alias. Exercitationem magni corporis sit ullam voluptatum delectus dolor? Soluta modi repudiandae ipsam?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, quaerat.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit.</p>
+        </div></Col>
+      
+    </Row>
+  </Container>
+  )
+}
+
+export default main
+
+
